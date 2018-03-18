0f1e1c9c04aaf131271d779b6b34a27f55a2c322
jc <jc@spoqa.com>
2018-03-18 14:47:16 +0900

    Add better shell

---
+++ shell.py
@@ -0,0 +1,18 @@
+import argparse
+import sys
+
+from ptpython.repl import embed
+
+
+def run():
+    from app import app
+
+    parser = argparse.ArgumentParser(description='Nekoyume shell')
+    args = parser.parse_args()
+
+    app.app_context().push()
+    embed(globals(), locals())
+
+
+if __name__ == '__main__':
+    run()

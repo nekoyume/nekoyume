d1e18eb1ad611bc2713b01955e1e3498150ff38a
jc <jc@spoqa.com>
2018-03-25 00:39:07 +0900

    Delete /node endpoint test temporarily

--- tests/api_test.py
+++ tests/api_test.py
@@ -3,19 +3,6 @@ import pytest
 from nekoyume.models import Node
 
 
-def test_post_node(fx_other_test_client, fx_session, fx_server,
-                   fx_other_session):
-    rv = fx_other_test_client.post('/nodes', data={
-        'url': fx_server.url
-    }, follow_redirects=True)
-
-    assert rv.status == '200 OK'
-
-    assert fx_other_session.query(Node).filter_by(
-        url=fx_server.url
-    ).first()
-
-
 def test_get_blocks(fx_test_client, fx_user):
     block = fx_user.create_block([])
 

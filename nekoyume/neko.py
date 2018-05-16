108e8121e50ba21c1802dd35574afb145f2db31d
jc <jc@spoqa.com>
2018-05-16 10:12:09 +0900

    Add Move & Block validation process

--- nekoyume/neko.py
+++ nekoyume/neko.py
@@ -8,7 +8,9 @@ from nekoyume.app import app
 def run():
     app.app_context().push()
     while True:
-        block = User('test').create_block(Move.query.filter_by(block=None))
+        block = User('test').create_block(
+            Move.query.filter_by(block=None).limit(20).all()
+        )
         if block:
             block.broadcast()
             print(block)

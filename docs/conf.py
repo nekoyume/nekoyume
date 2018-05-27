32bc69080ed6478b3390824aca2166a78ea7b427
jc <jc@spoqa.com>
2018-05-27 22:23:01 +0900

    0.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'JC Kim'
 # The short X.Y version
 version = '0.0'
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2'
 
 
 # -- General configuration ---------------------------------------------------

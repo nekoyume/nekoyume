31575a2339bf38104a730c1751c07d211d79291d
JC Kim <jckdotim@gmail.com>
2018-06-18 22:54:13 +0900

    0.0.3

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'JC Kim'
 # The short X.Y version
 version = '0.0'
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+release = '0.0.3'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='nekoyume',
-    version='0.0.2',
+    version='0.0.3',
     description='Decentralized MMORPG based on Dungeon World',
     long_description=long_description,
     url='https://github.com/nekoyume/nekoyume',

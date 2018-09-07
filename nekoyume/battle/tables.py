b415891f2a1da69d3a7bce2784372207af0e8a4d
incl <lee.incl@gmail.com>
2018-09-07 21:40:22 +0900

    Feature Table multi-load

--- nekoyume/battle/tables.py
+++ nekoyume/battle/tables.py
@@ -5,9 +5,17 @@ class TableData(dict):
         for i in range(len(header)):
             self[header[i]] = data[i]
 
+
 class Table(dict):
     separator = '\t'
     def __init__(self, filename):
+        if type(filename) == str:
+            self.load(filename)
+        elif type(filename) == list:
+            for f in filename:
+                self.load(f)
+    
+    def load(self, filename):
         dirname = os.path.join('battle', 'table')
         f = open(os.path.join(dirname, filename), 'r')
         text = f.read()
@@ -18,7 +26,9 @@ class Table(dict):
             data = line.split(Table.separator)
             self[data[0]] = TableData(header, data)
 
+
 class Tables:
     stats = Table('stats.tsv')
     monsters = Table('monsters.tsv')
-    skills = Table('skills.tsv')
+    skills = Table(['skills.tsv', 'monster_skills.tsv'])
+    items = Table(['items.tsv', 'item_weapons.tsv'])

a8a87c7e544d2c4dd493e8585d2827c34200a384
Seunghun Lee <waydi1@gmail.com>
2018-11-05 22:22:48 +0900

    Use setup.cfg in Dockerfile

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
 
+def print_install_requires():
+    import configparser;
+    c = configparser.ConfigParser();
+    c.read('setup.cfg');
+    print(c['options']['install_requires'])
+
 if __name__ == '__main__':
     setup()
